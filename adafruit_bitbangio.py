d64080d2fa0a2c8499ae5872cceabf89e5da3c6d
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_bitbangio.py
+++ adafruit_bitbangio.py
@@ -35,7 +35,7 @@ except ImportError:
 from time import monotonic
 from digitalio import DigitalInOut
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BitbangIO.git"
 
 MSBFIRST = 0
